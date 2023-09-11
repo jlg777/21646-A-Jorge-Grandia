237bab95e29180f4c41e9a9ccf6ae85666b89102
jlg777 <jorgel.granda@gmail.com>
2023-09-10 21:23:28 -0300

    correccion errores

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { taskRouter } from './src/routes/task.routes.js';
 import { startDb } from './src/config/database.js';
+import  cors  from 'cors'
 
 const app = express();
 
 //middlewares
 app.use(express.json())
+app.use(cors())
 
 const port = 3000;
 
